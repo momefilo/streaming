c365250c10b385dd0a2301cf33d07b1d6c7373c7
momefilo <106985138+momefilo@users.noreply.github.com>
2025-01-25 14:32:17 +0100

    Add files via upload

---
+++ stream_pic_csi.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+read key < /home/momefilo/streaming/key_pi4
+ffmpeg -re  -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 -loop 1 -i /home/momefilo/streaming/bild.jpg -vcodec h264 -pix_fmt yuv420p -preset ultrafast -g 20 -b:v 2500k -bufsize 512k -b:a 160k -c:a aac -ac 2 -ar 44100 -f flv rtmp://a.rtmp.youtube.com/live2/"$key"
